dacebbe1302e187582f861fb30345f881d7c3f82
EDRES2002 <edres.bensaied@moeen.ly>
2026-05-02 15:34:26 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/team', async (req, res) => {
+  const apiKey = req.headers['x-api-key'];
+  const r = await fetch('https://api.clickup.com/api/v2/team', {
+    headers: { Authorization: apiKey }
+  });
+  const data = await r.json();
+  res.json(data);
+});
+
+app.get('/tasks/:teamId', async (req, res) => {
+  const apiKey = req.headers['x-api-key'];
+  const r = await fetch(`https://api.clickup.com/api/v2/team/${req.params.teamId}/task?statuses[]=open&statuses[]=in+progress&statuses[]=to+do&page=0&limit=50`, {
+    headers: { Authorization: apiKey }
+  });
+  const data = await r.json();
+  res.json(data);
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Proxy running'));
