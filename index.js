07f2c3a3fbe9c7a86e72d55d9a8426e9c82a0010
EDRES2002 <edres.bensaied@moeen.ly>
2026-05-02 16:19:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,25 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const path = require('path');
 
 const app = express();
+app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/team', async (req, res) => {
-  const apiKey = req.query.key || req.headers['x-api-key'];
-  try {
-    const r = await fetch('https://api.clickup.com/api/v2/team', {
-      headers: { Authorization: apiKey }
-    });
-    const data = await r.json();
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Headers', '*');
-    res.json(data);
-  } catch(e) {
-    res.json({ err: e.message });
-  }
+  const apiKey = req.headers['x-api-key'];
+  const r = await fetch('https://api.clickup.com/api/v2/team', {
+    headers: { Authorization: apiKey }
+  });
+  res.json(await r.json());
 });
 
 app.get('/tasks/:teamId', async (req, res) => {
-  const apiKey = req.query.key || req.headers['x-api-key'];
-  try {
-    const r = await fetch(`https://api.clickup.com/api/v2/team/${req.params.teamId}/task?statuses[]=open&statuses[]=in+progress&statuses[]=to+do&page=0&limit=50`, {
-      headers: { Authorization: apiKey }
-    });
-    const data = await r.json();
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Headers', '*');
-    res.json(data);
-  } catch(e) {
-    res.json({ err: e.message });
-  }
+  const apiKey = req.headers['x-api-key'];
+  const r = await fetch(`https://api.clickup.com/api/v2/team/${req.params.teamId}/task?statuses[]=open&statuses[]=in+progress&statuses[]=to+do&page=0&limit=50`, {
+    headers: { Authorization: apiKey }
+  });
+  res.json(await r.json());
 });
 
-app.options('*', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Headers', '*');
-  res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.sendStatus(200);
-});
-
-app.listen(process.env.PORT || 3000, () => console.log('Proxy running'));
+app.listen(process.env.PORT || 3000, () => console.log('Running'));
